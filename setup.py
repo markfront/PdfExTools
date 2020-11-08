9e1bf62e9cbe3895658ff36257f0486694faec4b
markfront <markfront@gmail.com>
2020-11-08 00:36:05 -0800

    uploaded v0.2.0 to test-pypi

--- setup.py
+++ setup.py
@@ -4,22 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='PdfExTools',
-    version='0.2.0',
-    author='Mark Front',
-    author_email='mark.front@gmail.com',
-    description='A Python package of extended tools to process pdf files',
+    name="pdfextools-20_mark.gu-front_20",
+    version="0.2.0",
+    author="Mark Front",
+    author_email="mark.front@gmail.com",
+    description="A set of extended tools to process pdf files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/markfront/PdfTools',
+    url="https://github.com/markfront/PdfExTools",
     packages=setuptools.find_packages(),
-    install_requires=['pdfplumber>=0.5'],
-    scripts=['PdfExTools'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    zip_safe=False
 )
